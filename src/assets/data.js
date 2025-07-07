f3b7bb6b31af1bc82e44b3802713c0679c1a916d
kisajaBeddawala <kisajab72@gmail.com>
2025-07-08 05:27:12 +0530

    css part

---
+++ src/assets/data.js
@@ -0,0 +1,43 @@
+export const daata = [
+    {
+        question: "What is the capital of France?",
+        Option1: "Berlin",
+        Option2: "Madrid",
+        Option3: "Paris",
+        Option4: "Rome",
+        ans:3,
+    }
+    ,
+    {
+        question: "What is the largest planet in our solar system?",
+        Option1: "Earth",
+        Option2: "Jupiter",
+        Option3: "Mars",
+        Option4: "Saturn",
+        ans:2,
+    },
+    {
+        question: "Who wrote 'To Kill a Mockingbird'?",
+        Option1: "Harper Lee",
+        Option2: "Mark Twain",
+        Option3: "Ernest Hemingway",
+        Option4: "F. Scott Fitzgerald",
+        ans:1,
+    },
+    {
+        question: "What is the chemical symbol for water?",
+        Option1: "H2O",
+        Option2: "CO2",
+        Option3: "O2",
+        Option4: "NaCl",
+        ans:1,
+    },
+    {
+        question: "What is the capital of Japan?",
+        Option1: "Seoul",
+        Option2: "Tokyo",
+        Option3: "Beijing",
+        Option4: "Bangkok",
+        ans:2,
+    }
+]
